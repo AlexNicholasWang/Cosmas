2a16c75c1aef303988eee9b1f85345fedc5deca7
maxwbregler <max.wes.bregler@gmail.com>
2026-06-18 22:55:01 -0400

    basic chatbot functionality

--- backend/prompt.py
+++ backend/prompt.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pathlib import Path
+from typing import List
 
 BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
@@ -26,3 +27,49 @@ def prompt(data: PromptRequest):
     promptText = constitution + "Here's the user's information: " + data.userData
     response = client.models.generate_content(model = "gemini-3.1-flash-lite", contents = promptText).text
     return response
+
+class ChatTurn(BaseModel):
+    role: str
+    text: str
+
+class StatelessChatRequest(BaseModel):
+    history: List[ChatTurn]
+    vertical: str
+    new_message: str
+
+@app.post("/api/chat")
+async def chat(request: StatelessChatRequest):
+    contents = []
+    with open((BASE_DIR / f"{request.vertical}Constitution.txt"), "r", encoding="utf-8") as file:
+        constitution = file.read()
+    contents.append(
+        types.Content(
+            role = "user",
+            parts=[types.Part.from_text(text=constitution)]
+        )
+    )
+    try:
+        
+        for turn in request.history:
+            contents.append(
+                types.Content(
+                    role=turn.role,
+                    parts=[types.Part.from_text(text=turn.text)]
+                )
+            )
+        contents.append(
+            types.Content(
+                role="user",
+                parts=[types.Part.from_text(text=request.new_message)]
+            )
+        )
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite",
+            contents=contents,
+            config=types.GenerateContentConfig(system_instruction=constitution)
+        )
+
+        return {"response": response.text}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
